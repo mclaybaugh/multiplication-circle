03f7ce1c5291e584e6a6e3530da41ff544fc31e8
Michael Claybaugh <mclaybaugh2@liberty.edu>
2019-05-02 13:52:46 -0400

    var to let, originPoints.map, subNumber simplify

--- multiplication-circle.js
+++ multiplication-circle.js
@@ -1,7 +1,4 @@
-/* Author: Michael Claybaugh
- * Purpose: Draw multiplication circle to Canvas (HTML5)
- *
- * canvas coords note:
+/* canvas coords note:
  * -,- | +,-
  * -----------
  * -,+ | +,+
@@ -22,9 +19,9 @@ function incAndDraw(elementId, incVal) {
 }
 
 function toggleAnimation() {
-  var button = document.getElementById('startStopButton');
+  let button = document.getElementById('startStopButton');
   if (button.innerHTML == 'Start Animation') {
-    var msPerFrame = Number(1000 / document.getElementById('framesPerSec').value);
+    let msPerFrame = Number(1000 / document.getElementById('framesPerSec').value);
     drawCanvasID = setInterval(incMultAndDraw, msPerFrame);
     button.innerHTML = 'Stop Animation';
   } else {
@@ -34,9 +31,9 @@ function toggleAnimation() {
 }
 
 function incMultAndDraw(decrement) {
-  var multiplierInput = document.getElementById('multiplier');
-  var multiplier = Math.round(Number(multiplierInput.value) * 1000);
-  var delta = Math.round(Number(document.getElementById('delta').value) * 1000);
+  let multiplierInput = document.getElementById('multiplier');
+  let multiplier = Math.round(Number(multiplierInput.value) * 1000);
+  let delta = Math.round(Number(document.getElementById('delta').value) * 1000);
   if (decrement === 'decrement') {
     multiplierInput.value = (multiplier - delta) / 1000;
   } else {
@@ -46,9 +43,9 @@ function incMultAndDraw(decrement) {
 }
 
 function incModAndDraw(decrement) {
-  var modulusInput = document.getElementById('modulus');
-  var modulus = Number(modulusInput.value);
-  var delta = 1;
+  let modulusInput = document.getElementById('modulus');
+  let modulus = Number(modulusInput.value);
+  let delta = 1;
   if (decrement === 'decrement') {
     modulusInput.value = modulus - delta;
   } else {
@@ -98,56 +95,54 @@ window.onload = function initCanvas() {
   drawCanvas();
 };
 
-function Coord (x, y) {
-  this.x = x;
-  this.y = y;
-}
-
 function getSubNumber (multiplier) {
-  var subNumber;
   if (multiplier - Math.floor(multiplier) == 0) {
-    subNumber = 1;
+    return 1;
   }
   else if (multiplier * 10 - Math.floor(multiplier * 10) == 0) {
-    subNumber = 10;
+    return 10;
   }
   else if (multiplier * 100 - Math.floor(multiplier * 100) == 0) {
-    subNumber = 100;
+    return 100;
   }
-  else { subNumber = null; }
-  return subNumber;
+  else if (multiplier * 1000 - Math.floor(multiplier * 1000) == 0) {
+    return 1000;
+  }
+  else if (multiplier * 10000 - Math.floor(multiplier * 10000) == 0) {
+    return 10000;
+  }
+  else { return null; }
 }
 
-function getPointsArray (modulus, radius, subNumber) {
-  if (typeof subNumber === 'undefined') {
-    subNumber = 1;
+function getPoints(modulus, radius, subNumber = 1) {
+  let points = [];
+  let i = 0;
+  for (let angle = 0; angle < TAU; angle += (TAU / (modulus * subNumber))) {
+    points[i] = {
+      x: radius * Math.cos(angle),
+      y: radius * Math.sin(angle)
+    };
+    i++;
   }
-  var subPointsArray = [];
-  var kindex = 0;
-  for (var angle = 0; angle < TAU; angle = angle + (TAU / (modulus * subNumber))) {
-    subPointsArray[kindex] = new Coord(radius * Math.cos(angle),
-      radius * Math.sin(angle));
-    kindex++;
-  }
-  return subPointsArray;
+  return points;
 }
 
 function drawCanvas () {
   /****************************************************************
    * Step 1: get values from input fields and calculate coords
    * **************************************************************/
-  var dotRadius = document.getElementById('dotRadius').value;
-  var modulus = document.getElementById('modulus').value;
-  var multiplier = document.getElementById('multiplier').value;
-  var pointsArray = getPointsArray(modulus, radius);
-  var subNumber = getSubNumber(multiplier);
+  let dotRadius = document.getElementById('dotRadius').value;
+  let modulus = document.getElementById('modulus').value;
+  let multiplier = document.getElementById('multiplier').value;
+  let originPoints = getPointsArray(modulus, radius);
+  let subNumber = getSubNumber(multiplier);
   if (subNumber === null) {
-    var errorMessage = 'Multiplier of \'' + multiplier.toString() + '\' is not supported.';
+    let errorMessage = 'Multiplier of \'' + multiplier + '\' is not supported.';
     ctx.clearRect(-(cheight2), -(cheight2), canvas.height*2, canvas.height*2);
     ctx.fillText(errorMessage, -(cheight2) + 15, -(cheight2) + 15);
     return;
   }
-  var subPointsArray = getPointsArray(modulus, radius, subNumber);
+  let destPoints = getPointsArray(modulus, radius, subNumber);
 
   /****************************************************************
    * Step 2: Draw the dots and lines on the canvas
@@ -156,26 +151,25 @@ function drawCanvas () {
   ctx.clearRect(-(cheight2), -(cheight2), canvas.height*2, canvas.height*2);
 
   // write the values
-  var modString = 'Mod:  ' + modulus.toString();
-  var multString = 'Mult: ' + multiplier.toString();
+  let modString = 'Mod:  ' + modulus.toString();
+  let multString = 'Mult: ' + multiplier.toString();
   ctx.fillText(modString, -(cheight2) + 15, -(cheight2) + 20);
   ctx.fillText(multString, -(cheight2) + 15, -(cheight2) + 40);
 
   // draw dots
-  for (var coord in pointsArray) {
+  originPoints.map((coord) => {
     ctx.beginPath();
-    ctx.arc(pointsArray[coord].x, pointsArray[coord].y, dotRadius, 0, TAU);
+    ctx.arc(coord.x, coord.y, dotRadius, 0, TAU);
     ctx.fill();
-  }
+  });
 
   // draw lines
-  var smallIndex = 0;
-  for (var index = 0; index < subPointsArray.length; index = index + subNumber) {
-    // round endpoint equation to avoid errors with numbers like 10.00001
-    var endpoint = Math.round((index * multiplier) % (modulus * subNumber));
+  let smallIndex = 0;
+  for (let index = 0; index < destPoints.length; index += subNumber) {
+    let endpoint = Math.round((index * multiplier) % (modulus * subNumber));
     ctx.beginPath();
-    ctx.moveTo(pointsArray[smallIndex].x, pointsArray[smallIndex].y);
-    ctx.lineTo(subPointsArray[endpoint].x, subPointsArray[endpoint].y);
+    ctx.moveTo(originPoints[smallIndex].x, originPoints[smallIndex].y);
+    ctx.lineTo(destPoints[endpoint].x, destPoints[endpoint].y);
     ctx.stroke();
     smallIndex++;
   }
